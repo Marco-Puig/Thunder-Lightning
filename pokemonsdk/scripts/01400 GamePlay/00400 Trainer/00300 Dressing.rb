29cd3dd3b7454e745fc4fa2a39169062d6fbfa28
Marco Puig <dpuig2021@fau.edu>
2021-09-23 20:04:46 -0400

    Revert "Commit #4-2"
    This reverts commit 8b6a1ffe571f7f8d96704dfef93219f172f99546.

--- pokemonsdk/scripts/01400 GamePlay/00400 Trainer/00300 Dressing.rb
+++
@@ -1,1 +0,0 @@
-# TODO: Write the dresing scene (putting custom clothes / changing the appearence)
--- pokemonsdk/scripts/tools/GameLoader/0_fix_update.rb
+++
@@ -1,2 +0,0 @@
-# The purpose of this file is to fix binary file in order to ensure PSDK will run properly
-Dir['lib/*.update'].each { |filename| File.rename(filename, filename.sub('.update', '')) }
